4221df18905e6c97f96d9abbff92505c0fa11ec6
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-05-15 18:38:09 +0200

    feat: add object array for category

--- index.php
+++ index.php
@@ -3,15 +3,16 @@ require_once __DIR__ . '/Models/Product.php';
 require_once __DIR__ . '/Models/Food.php';
 
 //instanze di Product
-
+$plushDog = new Product('dd', 'Active Dog', 'peluche scoiattolo', 3.99,);
+$catnipBallCat = new Product('dd', 'Dynamicat', 'Pallina di erba gatta', 4.99,);
 
 //istanze di Food figlia di Product
 $mongeDogMoist = new Food('dd', 'Monge', 'Cibo umido con alto apporto nutritivo', 9.99, '250g', 'umido' );
 $NaturalTrainerCatDry = new Food('dd', 'Natural Trainer', 'Crocchette con alto apporto nutritivo per gatti sterilizzati', 19.99, '1000g', 'secco' );
 
 //istanze di Kennel figlia di Product
-$waterproofDogCoat = new Kennel('dd', 'Coat', 'cappottino impermeabile', 14.99, 'L',);
-$sweaterCat = new Kennel('dd', 'Sweat Cat', 'Maglioncino per gatto', 12.99, 'S');
+$waterproofDogCoat = new Kennel('dd', 'Cuccia Via', 'cuccia per cane di grossa taglia', 14.99, 'L',);
+$sweaterCat = new Kennel('dd', 'Gattuccia', 'cuccia per gatto', 12.99, 'S');
 
 
 
@@ -19,11 +20,13 @@ $sweaterCat = new Kennel('dd', 'Sweat Cat', 'Maglioncino per gatto', 12.99, 'S')
 
 //array che raccolgono prodotti per categorie: cane o gatto
 $dogProducts = [
+    $plushDog,
     $mongeDogMoist,
     $waterproofDogCoat,
 ];
 
 $catProducts = [
+    $catnipBallCat,
     $NaturalTrainerCatDry,
     $sweaterCat,
 ];
